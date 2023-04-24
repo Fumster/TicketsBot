7ed5574a7ec8bbd8e9e09112daf7a370dd80d5d7
amir1 <amir19040268@gmail.com>
2023-04-24 16:07:22 +0300

    implemented the method acceptIssue

--- workWithDB.py
+++ workWithDB.py
@@ -1,5 +1,6 @@
 import psycopg2
 import configparser
+from psycopg2 import Error
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
@@ -17,6 +18,31 @@ def createNewIssue():
     return
 
 
-def acceptIssue():
-    return
+def acceptIssue(msg_id, user_id):
+    try:
+        cursor = con.cursor()
+        # print("Таблица до обновления записи")
+        sql_select_query = """select * from applications where channel_msg_id  = %s"""
+        cursor.execute(sql_select_query, (msg_id,))
+        record = cursor.fetchone()
+        print(record)
 
+        # Обновление отдельной записи
+        sql_update_query = """Update applications set issuer = %s where channel_msg_id = %s"""
+        cursor.execute(sql_update_query, (user_id,msg_id))
+        con.commit()
+        print("Заказ быд принять сотрудником АСУ")
+
+        # print("Таблица после обновления записи")
+        sql_select_query = """select * from applications where channel_msg_id = %s"""
+        cursor.execute(sql_select_query, (msg_id,))
+        record = cursor.fetchone()
+        print(record)
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if con:
+            cursor.close()
+            con.close()
+            print("Соединение с PostgreSQL закрыто")
